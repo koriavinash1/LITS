aad73e1fd108edd3837d0a8dba325fe202644c29
KRISHNA CHAITANYA KALUVA <krishna.krishna.k75@gmail.com>
2017-07-07 09:56:13 +0530

    Committing th changes to the network

--- net/2d_net.py
+++ net/2d_net.py
@@ -45,7 +45,8 @@ class Tiramisu(object):
 				idx = gpu_ids.index(i)
 				with tf.name_scope('tower_%d'%idx):
 					with tf.device('/gpu:%d'%i):
-						self.doForward(n_feat_first_layer = n_feat_first_layer,
+						self.doForward(idx,
+										n_feat_first_layer = n_feat_first_layer,
 										n_pool = n_pool,
 										growth_rate = growth_rate,
 										n_layers_per_block = n_layers_per_block,
@@ -59,5 +60,146 @@ class Tiramisu(object):
 		self.addSummaries()
 
 
-	def doForward(n_feat_first_layer = n_feat_first_layer, n_pool = n_pool, growth_rate = growth_rate, n_layers_per_block = n_layers_per_block, keep_prob = keep_prob):
+	def doForward(idx, n_feat_first_layer, n_pool, growth_rate, n_layers_per_block, keep_prob):
 		
+		assert type(n_layers_per_block) == int
+		n_layers_per_block = [n_layers_per_block] * (2 * n_pool + 1)
+
+		inputs = self.image_splits[idx]
+		tf.summary.image('inputs',inputs[:,:,:,1:2], max_outputs = 4, collections = ['per_100_steps'])
+
+		with tf.variable_scope('input_layer'):
+			stack = Conv2D(inputs, [3,3,inputs.get_shape()[-1].value,n_filters_first_conv],collection_name = 'input_layer', padding='SAME')
+
+		n_filters = n_filters_first_conv
+
+		skip_connection_list = []
+
+		#######################
+		#   Downsampling path   #
+		#######################
+
+		for i in range(n_pool):
+			for j in range(n_layers_per_block[i]):
+				with tf.variable_scope('dense' + str(i) + '_' + str(j)):
+					l = BN_eLU_Conv(stack, growth_rate,collection_name = 'block_%d_layer_%d'%(i,j), keep_prob=keep_prob, is_training=self.is_training)
+					stack = tf.concat([stack, l],3)
+				n_filters += growth_rate
+
+			skip_connection_list.append(stack)
+
+			with tf.variable_scope('dense' + str(i) + '_TD'):
+				stack = TransitionDown(stack, n_filters,collection_name = 'transition_%d'%(i),keep_prob=keep_prob, is_training=self.is_training)
+
+		skip_connection_list = skip_connection_list[::-1]
+		block_to_upsample = []
+
+		for j in range(n_layers_per_block[n_pool]):
+			with tf.variable_scope('densemid' + str(j)):
+				l = BN_eLU_Conv(stack, growth_rate, collection_name = 'bottle_neck_layer', keep_prob=keep_prob, is_training=self.is_training)
+				block_to_upsample.append(l)
+				stack = tf.concat([stack, l],3)
+
+		#######################
+		#   Upsampling path   #
+		#######################
+
+		for i in range(n_pool):
+			with tf.variable_scope('transitionup' + str(i)):
+				n_filters_keep = growth_rate * n_layers_per_block[n_pool + i]
+				stack = TransitionUp(skip_connection_list[i], block_to_upsample, n_filters_keep,collection_name = 'transition_%d'%(n_pool-i-1), is_training=self.is_training)
+
+			block_to_upsample = []
+			for j in range(n_layers_per_block[n_pool + i + 1]):
+				with tf.variable_scope('transitionup' + str(i) + '_dense' + str(j)):
+					l = BN_eLU_Conv(stack, growth_rate, collection_name = 'block_%d_layer_%d'%(n_pool-i-1,j), keep_prob=keep_prob, is_training=self.is_training)
+					block_to_upsample.append(l)
+					stack = tf.concat([stack, l],3)
+
+		#####################
+		# 		Outputs 	#
+		#####################
+
+		with tf.variable_scope('logits'):
+			self.logits[idx] = Conv2D(stack, [3,3,stack.get_shape()[-1].value,self.num_classes], collection_name = 'logits_layer', padding='SAME')
+
+		with tf.variable_scope('logits'):
+			self.posteriors[idx] = Softmax(self.logits[idx])	
+
+		with tf.variable_scope('predictions'):
+			self.predictions[idx] = tf.cast(tf.argmax(self.logits[idx],3), tf.float32)
+			tf.summary.image('predictions',self.predictions[idx][:,:,:,None], max_outputs = 4, collections = ['per_100_steps'])
+
+	def buildInferences(self,gpu_id_idx):
+		for metric_name in self.metrics_list:
+			metric_implemented = False
+			if metric_name == 'loss':
+				metric_implemented = True
+				with tf.variable_scope(metric_name):
+					self.inference_ops[gpu_id_idx][metric_name] = loss = SpatialWeightedCrossEntropyLogits(self.logits[gpu_id_idx], self.labels_splits[gpu_id_idx],self.weight_splits[gpu_id_idx]) 
+					#class weights should be in float
+					metric_obj = ScalarMetricStream(loss)
+
+					tf.summary.scalar('per-step',metric_obj.op,collections = ['per_step'])
+					tf.summary.scalar('epoch-avg',metric_obj.avg,collections = ['per_epoch'])					
+
+			elif metric_name == 'dice_score':
+				metric_implemented = True
+				with tf.variable_scope(metric_name):
+					self.inference_ops[gpu_id_idx][metric_name] = dice_score = DiceCriteria2Cls(self.logits[gpu_id_idx],self.labels_splits[gpu_id_idx],chief_class = self.chief_class)
+					metric_obj = ScalarMetricStream(dice_score)
+					tf.summary.scalar('per-step',metric_obj.op,collections = ['per_step'])
+					tf.summary.scalar('epoch-avg',metric_obj.avg,collections = ['per_epoch'])
+
+			else:
+				print('Error : ' + metric_name + ' is not implemented')
+			
+			if metric_implemented == True: 
+				try:		
+					self.accumulate_ops[gpu_id_idx].append(metric_obj.accumulate)
+					self.stats_ops[gpu_id_idx][metric_name] = metric_obj.stats
+					self.reset_ops[gpu_id_idx].append(metric_obj.reset)
+				except AttributeError:
+					pass
+
+		l2_norm  = sum([tf.nn.l2_loss(v) for v in tf.get_collection('l2_norm_vars')])
+		total_loss = self.inference_ops[gpu_id_idx][self.metric_to_optimize] + self.weight_decay*l2_norm
+		with tf.variable_scope('gradients'):
+			grads = self.optimizer.compute_gradients(total_loss)
+		
+		self.grads_dict[gpu_id_idx] = grads
+		grads = None
+
+
+	def _averageGradients(self,grads_list):
+		average_grads = []
+		for grad_and_vars in zip(*grads_list):
+			grads = []
+			for g, _ in grad_and_vars:
+				expanded_g = tf.expand_dims(g, 0)
+				grads.append(expanded_g)
+				expanded_g = None
+
+			grad = tf.concat(grads, 0)
+			grad = tf.reduce_mean(grad, 0)
+
+			v = grad_and_vars[0][1]
+			grad_and_var = (grad, v)
+			average_grads.append(grad_and_var)
+			grad_and_var = None
+		return average_grads
+
+
+	def optimize(self):
+		with tf.variable_scope('average_gradients'):
+			grads = self._averageGradients(self.grads_dict.values())
+
+		with tf.variable_scope('update_op'):
+			self.update_op = self.optimizer.apply_gradients(grads)
+
+
+	def makeSummaries(self):
+		self.summary_ops = {}		
+		self.summary_ops['1step'] = tf.summary.merge_all(key = 'per_step')
+		self.summary_ops['100steps'] = tf.summary.merge_all(key = 'per_100_steps')	
+		self.summary_ops['1epoch'] = tf.summary.merge_all(key = 'per_epoch')
--- utils/train_utils.py
+++ utils/train_utils.py
@@ -102,6 +102,62 @@ def BatchNorm(inputs, bn_mode, decay = 0.9, epsilon=1e-3, collections = []):
 def ConvEluBatchNormDropout(x, shape, stride = 1,padding = 'VALID', bn_mode = tf.placeholder_with_default(False, shape = []), drop_mode = tf.placeholder_with_default(False, shape = []), keep_prob = 0.7, collections = []):
     return Dropout(BatchNorm(ConvElu(x,shape,stride,padding, collections = collections),bn_mode, collections = collections), drop_mode,keep_prob)
 
+def TransitionDown(inputs, n_filters,collection_name, keep_prob=0.8, is_training=tf.constant(False,dtype=tf.bool)):
+    """ Apply first a BN_ReLu_conv layer with filter size = 1, and a max pooling with a factor 2  """
+    l = BN_eLU_Conv(inputs, n_filters,collection_name=collection_name, filter_size=1, keep_prob=keep_prob, is_training=is_training)
+    l = MaxPool2(l)
+
+    return l
+
+def TransitionUp(skip_connection, block_to_upsample, n_filters_keep,collection_name, is_training=tf.constant(False,dtype=tf.bool)):
+    """
+    Performs upsampling on block_to_upsample by a factor 2 and concatenates it with the skip_connection """
+    l = tf.concat(block_to_upsample,3)
+    l = SpatialBilinearUpsampling(l)
+    l = BatchNorm(Conv2D(l, [3,3,l.get_shape()[-1].value,n_filters_keep],collection_name = collection_name, padding='SAME'), is_training)
+    l = tf.concat([l, skip_connection],3)
+
+    return l
+
+def BN_eLU_Conv(inputs, n_filters,collection_name, filter_size=3, keep_prob=0.8, is_training=tf.constant(False,dtype=tf.bool)):
+    l = Elu(BatchNorm(inputs,is_training=is_training))
+    l = Conv2D(l, [filter_size, filter_size, l.get_shape()[-1].value, n_filters],collection_name = collection_name, padding='SAME')
+    l = Dropout(l, is_training=is_training,keep_prob=keep_prob)
+    return l
+
+def SpatialWeightedCrossEntropyLogits(logits, targets, weight_map, name='spatial_wx_entropy'):
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels = targets,logits = logits)
+    weighted_cross_entropy = tf.multiply(cross_entropy, weight_map)
+    mean_weighted_cross_entropy = tf.reduce_mean(weighted_cross_entropy, name=name)
+    return mean_weighted_cross_entropy
+
+def DiceCriteria2Cls(logits, targets, chief_class, smooth = 1.0, name = 'dice_score'):
+    last_dim_idx = logits.get_shape().ndims - 1
+    num_class = tf.shape(logits)[last_dim_idx]
+    predictions = tf.one_hot(tf.argmax(logits,last_dim_idx),num_class)
+    preds_unrolled = tf.reshape(predictions,[-1,num_class])[:,chief_class]
+    targets_unrolled = tf.reshape(targets,[-1,num_class])[:,chief_class]
+    intersection = tf.reduce_sum(preds_unrolled*targets_unrolled)
+    ret_val = (2.0*intersection)/(tf.reduce_sum(preds_unrolled)
+     + tf.reduce_sum(targets_unrolled) + smooth)
+    ret_val = tf.identity(ret_val,name = 'dice_score')
+    return ret_val
+
+class ScalarMetricStream(object):
+    def __init__(self,op, filter_nan = False):
+        self.op = op
+        count = tf.constant(1.0)
+        self.sum = tf.Variable([0.0,0], name = op.name[:-2] + '_sum', trainable = False)
+        self.avg = tf.Variable(0.0, name = op.name[:-2] + '_avg', trainable = False)
+
+        if filter_nan == True:
+            op_is_nan = tf.is_nan(self.op)
+            count = tf.cond(op_is_nan, lambda : tf.constant(0.0), lambda : tf.constant(1.0))
+            self.op = tf.cond(op_is_nan, lambda : tf.constant(0.0),lambda : tf.identity(self.op))
+        self.accumulate = tf.assign_add(self.sum,[self.op,count])
+        self.reset = tf.assign(self.sum,[0.0,0.0])
+        self.stats = tf.assign(self.avg,self.sum[0]/(0.001 + self.sum[1]))
+
 def Adam(lr):
     return tf.train.AdamOptimizer(learning_rate = lr)
 
